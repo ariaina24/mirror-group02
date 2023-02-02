286c81d945d7cc985d3b4d155e3393aa0791e6c5
Njiva Ariaina <njivaariaina47@gmail.com>
2023-02-03 10:26:06 +1200

    Re-création du tableau et Ajout de Bootstrap 4 pour une bonne mise en forme du tableau

--- server.py
+++ server.py
@@ -21,20 +21,22 @@ class index:
         cstm=db.select('Customer', limit=10)
         playList=db.select('Playlist', limit=10)
         employe=db.select('Employee', limit=10)
-        result = '<html><head><title>test</title></head>'
-        result += '<table border= "1">'
-        result += '<tr><th>Id_Artist</th><th>Artists</th><th>Genres</th><th>Playlists</th><th>Albums</th></tr>'
+        result = '<html><head><title>test</title>'
+        result += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">'
+        result += '</head>'
+        result += '<table class="container table table-border" border= "1">'
+        result += '<tr class="bg-primary text-white text-center"><th>Id Artist</th><th>Artists</th><th>Genres</th><th>Customer</th><th>Employee</th><th>Playlists</th><th>Albums</th></tr>'
         for a in a2:
             result += '<tr>'
             for artist in a3:
-                result += '<td>' + str(a.ArtistId) + '</td>'
+                result += '<td class="bg-dark text-white text-center">' + str(a.ArtistId) + '</td>'
                 result += '<td>' +artist.Name+'</td>'
                 break 
             for genre in grs:
                 result += '<td>' +genre.Name+'</td>'
                 break
             for customer in cstm:
-                result += '<td>' +customer.FirstName+'</td>'
+                result += '<td>' +customer.FirstName+'</td>' 
                 break
             for employee in employe:
                 result += '<td>' +employee.LastName+'</td>'
