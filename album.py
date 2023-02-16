a0602d8f1c26c24a5b7f2cb58ba8a36f71b5c380
Njiva Ariaina <njivaariaina47@gmail.com>
2023-02-16 02:57:52 -1200

    Redimension du taableau et centrage de tout les éléments dans le tableau

--- album.py
+++ album.py
@@ -20,12 +20,12 @@ class Album:
         db = d.getDb()
         albums = db.select('Album', limit=8)
         result = navbar_html
-        result += '<div class="container-fluid">'
-        result += '<table class="table table-border">'
+        result += '<div class="container">'
+        result += '<table class="table table-border w-75 m-auto">'
         result += '<thead class="bg-primary text-white">'
         result += '<tr>'
-        result += '<th>#</th>'
-        result += '<th>Album List</th>'
+        result += '<th class="text-center">#</th>'
+        result += '<th class="text-center">Album List</th>'
         result += '</tr>'
         result += '</thead>'
         result += '<tbody>'
@@ -33,7 +33,7 @@ class Album:
             albums = db.select('Album', where='AlbumId=$id', vars={'id': album.AlbumId})
             result += '<tr>'
             result += '<td class="bg-dark text-white text-center">' + str(album.AlbumId) + '</td>'
-            result += '<td>' + album.Title + '</td>'
+            result += '<td class="text-center">' + album.Title + '</td>'
             result += '</tr>'
         result += '</tbody>'
         result += '</table>'
